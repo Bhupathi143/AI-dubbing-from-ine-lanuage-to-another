c585cf1f84c94383cc6aba8195bb8cb89dbc45a5
Kadiyala Bhupathi <kadiyalabhupathi325@gmail.com>
2025-10-05 10:39:49 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,20 +1,42 @@
 from django.shortcuts import render
-from users.forms import UserRegistrationForm
+from django.contrib import messages
+from users.models import UserRegistrationModel
 
+# Create your views here.
 
-def index(request):
-    return render(request, 'index.html', {})
-
-def logout(request):
-    return render(request, 'index.html', {})
-
-def UserLogin(request):
-    return render(request, 'UserLogin.html', {})
-
-def AdminLogin(request):
+def AdminLoginCheck(request):
+    if request.method == 'POST':
+        usrid = request.POST.get('loginid')
+        pswd = request.POST.get('pswd')
+        print("User ID is = ", usrid)
+        if usrid == 'admin' and pswd == 'admin':
+            return render(request, 'admins/AdminHome.html')
+        elif usrid == 'Admin' and pswd == 'Admin':
+            return render(request, 'admins/AdminHome.html')
+        else:
+            messages.success(request, 'Please Check Your Login Details')
     return render(request, 'AdminLogin.html', {})
 
 
-def UserRegister(request):
-    form = UserRegistrationForm()
-    return render(request, 'UserRegistrations.html', {'form': form})
+def AdminHome(request):
+    return render(request, 'admins/AdminHome.html')
+
+
+def ViewRegisteredUsers(request):
+    data = UserRegistrationModel.objects.all()
+    return render(request, 'admins/RegisteredUsers.html', {'data': data})
+
+
+def AdminActivaUsers(request):
+    if request.method == 'GET':
+        id = request.GET.get('uid')
+        status = 'activated'
+        print("PID = ", id, status)
+        UserRegistrationModel.objects.filter(id=id).update(status=status)
+        data = UserRegistrationModel.objects.all()
+        return render(request, 'admins/RegisteredUsers.html', {'data': data})
+
+# def AdminStressDetected(request):
+#     data = UserImagePredictinModel.objects.all()
+#     return render(request, 'admins/AllUsersStressView.html', {'data': data})
+
